9e1fb65af9fa08242161a8a6abcfcf1d3b78d1c0
Facu Areosa <facundo.areosa@gmail.com>
2023-05-19 11:11:02 +0200

    filter function OK

--- js/script.js
+++ js/script.js
@@ -6,48 +6,84 @@ window.onload = function() {
         .then(data => {
             data.games.forEach(gameData => {
                 createCard(gameData);
-                console.log("hola")
             });
-        })
+        });
 };
+
+const search = document.querySelector(".selector__submit");
+
+search.addEventListener("click", () => {
+    JSONfetch()
+        .then(data => {
+            const filterInput = document.querySelector(".selector__input");
+            const genreSelected = filterInput.value;
+            const gamesToDisplay = filterByGenre(data.games, genreSelected);
+            renderFilteredGames(gamesToDisplay);
+            filterInput.value = ""
+        });
+});
+
+function filterByGenre(data, genero) {
+    if (genero !== "Todos") {
+        return data.filter((game) => game.genre === genero);
+    }
+    return data;
+}
+
+function renderFilteredGames(filteredGames) {
+    const gameContainer = document.querySelector(".games__container");
+
+    while (gameContainer.firstChild) {
+        gameContainer.removeChild(gameContainer.firstChild);
+    }
+
+    filteredGames.forEach((game) => {
+        createCard(game);
+    });
+}
+
+// import { JSONfetch } from './component/jsonFetch.js';
+// import { createCard } from './component/createCard.js';
+
+// window.onload = function() {
+//     JSONfetch()
+//         .then(data => {
+//             data.games.forEach(gameData => {
+//                 createCard(gameData);
+//                 gameFilter(gameData)
+//             }); 
+//         })
+// };
 
 
 // const search = document.querySelector(".selector__submit");
 
 // search.addEventListener("click", ()=>{
-//     JSONfetch()
+//     gameFilter(gameData)
 // })
 
 
-// // function JSONfetch(data){
-// //     const filterInput = document.querySelector(".selector__input")
-// //     let genreSelected = filterInput.value
+// function gameFilter(gameData){
+//     const filterInput = document.querySelector(".selector__input")
+//     let genreSelected = filterInput.value
 
-// //     fetch("games.json")
-// //         .then(res => res.json())
-// //         .then(data => {
-// //             filterByGenre(data, genreSelected);
-// //         })
-// //         .then(() => {
-// //             filterInput.value = ""
-// //         })
-// // }
+//     filterByGenre(gameData, genreSelected);
+// }
 
-// // function filterByGenre(data,genero){
-// //     const gameContainer = document.querySelector(".games__container")
-// //     let gamesToDisplay = data.games
+// function filterByGenre(data,genero){
+//     const gameContainer = document.querySelector(".games__container")
+//     let gamesToDisplay = data
     
+//     while (gameContainer.firstChild) {
+//         gameContainer.removeChild(gameContainer.firstChild);
+//     }
     
-// //     while (gameContainer.firstChild) {
-// //         gameContainer.removeChild(gameContainer.firstChild);
-// //     }
+//     if (genero !== "Todos") {
+//         gamesToDisplay = data.filter((game) => game.genre === genero);
+//     }
     
-// //     if (genero !== "Todos") {
-// //         gamesToDisplay = data.games.filter((game) => game.genre === genero);
-// //     }
-    
-// //     gamesToDisplay.forEach((juego) => {
-// //         createCard(juego);
-// //     })
-// // }
+//         gamesToDisplay.forEach((juego) => {
+//             createCard(juego);
+//     })
+// }
 
