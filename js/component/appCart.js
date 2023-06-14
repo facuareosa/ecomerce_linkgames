77101ba98b7984e790d2daae45b3a07166f4475c
Facu Areosa <facundo.areosa@gmail.com>
2023-06-14 09:59:38 +0200

    empty cart function

--- js/component/appCart.js
+++ js/component/appCart.js
@@ -1,68 +1,79 @@
 export function shop() {
-    let gameShopSection = document.querySelector(".modal__article-container");
-  
-    let cartItems = localStorage.getItem("cartItems");
-    cartItems = cartItems ? JSON.parse(cartItems) : [];
-  
-    gameShopSection.innerHTML = "";
-  
-    let modalTotalPrice = [];
-    console.log(modalTotalPrice);
-  
-    let initialTotalPrice = 0; // Variable para almacenar la suma total inicial
-  
+  let gameShopSection = document.querySelector(".modal__article-container");
+
+  let cartItems = localStorage.getItem("cartItems");
+  cartItems = cartItems ? JSON.parse(cartItems) : [];
+
+  gameShopSection.innerHTML = "";
+
+  let modalTotalPrice = [];
+  console.log(modalTotalPrice);
+
+  let initialTotalPrice = 0;
+
+  if (cartItems.length === 0) {
+    const emptyCartTemplate = `
+    <div class="modal__empty">
+      <img class="modal__empty-cross" src="./assets/img/img_ico/cross.svg" alt="cross_ico">
+      <p class="modal__empty-text">Tu carrito está vacío</p>
+    </div>
+    `;
+
+    // Insertar el SVG y el mensaje en el elemento gameShopSection
+    gameShopSection.innerHTML = emptyCartTemplate;
+  } else {
     cartItems.forEach((product) => {
-        let shopDiv = document.createElement("div");
-        shopDiv.className = "modal__article";
-      
-        let { img, title, price } = product;
-      
-        let gameShopTemplate = `
-          <input type="number" class="modal__numbers" id="modal__numbers" name="modal__numbers" value="1" min="1" max="10">
-          <img src=${img} alt="" class="modal__img">
-          <h2 class="modal__game">${title}</h2>
-          <span class="modal__price">${price}</span>
-          <img src="./assets/img/img_ico/trash-filled.svg" class="modal__trash" alt="trashcan">
-        `;
-      
-        shopDiv.innerHTML = gameShopTemplate;
-      
-        gameShopSection.appendChild(shopDiv);
-      
-        let unitsInput = shopDiv.querySelector(".modal__numbers");
-        let unitPrice = parseFloat(product.price.slice(1));
-      
-        unitsInput.addEventListener("input", (event) => {
-          let newUnits = parseInt(event.target.value);
-          product.units = newUnits;
-      
-          modalTotalPrice = cartItems.map((item) => item.units * parseFloat(item.price.slice(1)));
-      
-          const totalPrice = modalTotalPrice.reduce((anterior, actual) => anterior + actual, 0);
-          document.getElementById("totalSpan").textContent = totalPrice;
-      
-          localStorage.setItem("cartItems", JSON.stringify(cartItems));
-        });
-      
-        let initialGamePrice = unitPrice * 1; // Calcula el precio inicial del juego (valor inicial del input = 1)
-        initialTotalPrice += initialGamePrice; // Suma el precio inicial al total inicial
-      
-        if (!product.hasOwnProperty("units")) {
-          product.units = 1; // Agrega la propiedad "units" con un valor de 1 si no existe
-        }
+      let shopDiv = document.createElement("div");
+      shopDiv.className = "modal__article";
+
+      let { img, title, price } = product;
+
+      let gameShopTemplate = `
+        <input type="number" class="modal__numbers" id="modal__numbers" name="modal__numbers" value="1" min="1" max="10">
+        <img src=${img} alt="" class="modal__img">
+        <h2 class="modal__game">${title}</h2>
+        <span class="modal__price">${price}</span>
+        <img src="./assets/img/img_ico/trash-filled.svg" class="modal__trash" alt="trashcan">
+      `;
+
+      shopDiv.innerHTML = gameShopTemplate;
+
+      gameShopSection.appendChild(shopDiv);
+
+      let unitsInput = shopDiv.querySelector(".modal__numbers");
+      let unitPrice = parseFloat(product.price.slice(1));
+
+      unitsInput.addEventListener("input", (event) => {
+        let newUnits = parseInt(event.target.value);
+        product.units = newUnits;
+
+        modalTotalPrice = cartItems.map((item) => item.units * parseFloat(item.price.slice(1)));
+
+        const totalPrice = modalTotalPrice.reduce((anterior, actual) => anterior + actual, 0);
+        document.getElementById("totalSpan").textContent = totalPrice;
+
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
       });
-      
-  
+
+      let initialGamePrice = unitPrice * 1; // Calcula el precio inicial del juego (valor inicial del input = 1)
+      initialTotalPrice += initialGamePrice; // Suma el precio inicial al total inicial
+
+      if (!product.hasOwnProperty("units")) {
+        product.units = 1; // Agrega la propiedad "units" con un valor de 1 si no existe
+      }
+    });
+
     const totalPrice = modalTotalPrice.reduce((anterior, actual) => anterior + actual, initialTotalPrice); // Calcula la suma total considerando el total inicial
     document.getElementById("totalSpan").textContent = totalPrice;
-  
-    if (modalTotalPrice.length > 0) {
-      const totalPrice = modalTotalPrice.reduce((anterior, actual) => anterior + actual, initialTotalPrice);
-      document.getElementById("totalSpan").textContent = totalPrice;
-    } else {
-      document.getElementById("totalSpan").textContent = initialTotalPrice;
-    }
   }
+
+  if (modalTotalPrice.length > 0) {
+    const totalPrice = modalTotalPrice.reduce((anterior, actual) => anterior + actual, initialTotalPrice);
+    document.getElementById("totalSpan").textContent = totalPrice;
+  } else {
+    document.getElementById("totalSpan").textContent = initialTotalPrice;
+  }
+}
   
 
 function removeFromCart(event) {
