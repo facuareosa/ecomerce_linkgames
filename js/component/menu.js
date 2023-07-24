da899df33d11e3882d0decbccf7e995ea6cfeeda
Facu Areosa <facundo.areosa@gmail.com>
2023-07-24 12:57:45 +0200

    finish menu mobile

--- js/component/menu.js
+++ js/component/menu.js
@@ -1,6 +1,24 @@
 export function displayMenu(){
 
-    let navBar =document.querySelector(".header__nav")
+    window.scrollTo({
+        top: 0,
+        behavior: "instant"
+    });
 
-    navBar.classList.toggle("header__nav-activo")
+    let navBar      = document.querySelector(".header__nav");
+    let menuIcon    = document.querySelector(".header__menu");
+    let headerUl    = document.querySelector(".header__ul");
+    let header      = document.querySelector(".header") 
+
+    navBar.classList.toggle("header__nav-activo");
+    menuIcon.classList.toggle("menu-activo");
+    headerUl.classList.toggle("header__ul-activo");
+    header.classList.toggle("header-activo")
+
+    if (navBar.classList.contains("header__nav-activo")) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "auto";
+    }
+
 }
